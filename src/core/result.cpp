b3297a5c9518a0ce3b1812dbd109e40ee4afd1da
Luko van der Maas <luko.vandermaas@ru.nl>
2026-04-17 11:23:40 +0200

    Remove dense tracker and fix scheduler

--- src/core/result.cpp
+++ src/core/result.cpp
@@ -84,7 +84,7 @@ void define_typed_result(py::module& m, std::string const& vtSuffix) {
                 return result[state];
             }, py::arg("state"), "Get result for given state")
         .def("get_truth_values", &storm::modelchecker::ExplicitQualitativeCheckResult<ValueType>::getTruthValuesVector, "Get BitVector representing the truth values")
-        .def_property_readonly("scheduler", [](storm::modelchecker::ExplicitQualitativeCheckResult const& res) {return res.getScheduler();}, "get scheduler")
+        .def_property_readonly("scheduler", [](storm::modelchecker::ExplicitQualitativeCheckResult<ValueType> const& res) {return res.getScheduler();}, "get scheduler")
     ;
 
     py::class_<storm::modelchecker::QuantitativeCheckResult<ValueType>, std::shared_ptr<storm::modelchecker::QuantitativeCheckResult<ValueType>>, storm::modelchecker::CheckResult> quantitativeCheckResult(m, ("_" + vtSuffix + "QuantitativeCheckResult").c_str(), "Abstract class for quantitative model checking results");

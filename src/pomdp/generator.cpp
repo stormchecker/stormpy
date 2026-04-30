569201e388d965b87acf3be8018ed43fcac020f5
Luko van der Maas <luko.vandermaas@ru.nl>
2026-04-30 16:28:07 +0200

    Refactor MonitorVerifier bindings to use reference_internal policy and add unit tests for monitor verifier functionality

--- src/pomdp/generator.cpp
+++ src/pomdp/generator.cpp
@@ -22,17 +22,16 @@ void define_verimon_generator(py::module &m, std::string const &vtSuffix) {
         m, ("MonitorVerifier" + vtSuffix).c_str(), "Container for monitor verifier POMDP with associated objects");
     mv.def(py::init<const SparsePomdp<ValueType> &, const std::map<std::pair<uint32_t, bool>, uint32_t> &, const std::map<uint32_t, std::string> &>(),
            py::arg("product"), py::arg("observation_map"), py::arg("default_action_map"));
-    mv.def("get_product", &storm::generator::MonitorVerifier<ValueType>::getProduct, py::return_value_policy::copy);
-    mv.def_property_readonly("observation_map", &storm::generator::MonitorVerifier<ValueType>::getObservationMap, py::return_value_policy::copy);
-    mv.def_property_readonly("default_action_map", &storm::generator::MonitorVerifier<ValueType>::getObservationDefaultAction, py::return_value_policy::copy);
+    mv.def("get_product", &storm::generator::MonitorVerifier<ValueType>::getProduct, py::return_value_policy::reference_internal);
+    mv.def_property_readonly("observation_map", &storm::generator::MonitorVerifier<ValueType>::getObservationMap, py::return_value_policy::reference_internal);
+    mv.def_property_readonly("default_action_map", &storm::generator::MonitorVerifier<ValueType>::getObservationDefaultAction, py::return_value_policy::reference_internal);
 
     py::class_<storm::generator::GenerateMonitorVerifier<ValueType>> gmv(m, ("GenerateMonitorVerifier" + vtSuffix).c_str(),
                                                                          "Generator of POMDP used in verifying monitors against markov chains");
     gmv.def(py::init<SparseDtmc<ValueType> const &, SparseMdp<ValueType> const &, std::shared_ptr<storm::expressions::ExpressionManager> &,
                      GenerateMonitorVerifierOptions<ValueType> const &>(),
             py::arg("mc"), py::arg("monitor"), py::arg("expr_manager"), py::arg("options"));
-    gmv.def("create_product", &storm::generator::GenerateMonitorVerifier<ValueType>::createProduct, py::return_value_policy::copy,
-            "Create the verification POMDP");
+    gmv.def("create_product", &storm::generator::GenerateMonitorVerifier<ValueType>::createProduct, "Create the verification POMDP");
     gmv.def("set_risk", &storm::generator::GenerateMonitorVerifier<ValueType>::setRisk, py::arg("risk"));
 
     py::class_<GenerateMonitorVerifierOptions<ValueType>> gmvopts(m, ("GenerateMonitorVerifier" + vtSuffix + "Options").c_str(),

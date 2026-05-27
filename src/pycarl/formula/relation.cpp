22e973dfb0094e9d8225bf143715fef9470bc428
Sebastian Junges <sebastian.junges@ru.nl>
2026-05-27 23:49:37 +0200

    change from enum_ to native_enum

--- src/pycarl/formula/relation.cpp
+++ src/pycarl/formula/relation.cpp
@@ -5,12 +5,13 @@
 #include "src/helpers.h"
 
 void define_relation(py::module& m) {
-    py::enum_<carl::Relation>(m, "Relation")
+    py::native_enum<carl::Relation>(m, "Relation", "enum.Enum")
         .value("EQ", carl::Relation::EQ)
         .value("NEQ", carl::Relation::NEQ)
         .value("LESS", carl::Relation::LESS)
         .value("LEQ", carl::Relation::LEQ)
         .value("GREATER", carl::Relation::GREATER)
         .value("GEQ", carl::Relation::GEQ)
-        .def("__str__", &streamToString<carl::Relation>, py::prepend() /* use custom method instead of default enum overload */);
+        .def("friendly_name", &streamToString<carl::Relation>)
+        .finalize();
 }
